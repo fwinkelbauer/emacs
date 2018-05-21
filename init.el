59f2ddf3ab798433798f24b0e240c94394a66d26
Florian Winkelbauer <mail@florianwinkelbauer.com>
2018-05-21 16:16:51 +0200

    Add custom faces for markdown-mode

--- init.el
+++ init.el
@@ -1,3 +1,16 @@
 (package-initialize)
 
 (org-babel-load-file "~/.emacs.d/README.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(markdown-code-face ((t (:inherit fixed-pitch :background "#ffffff" :foreground "#8959a8" :height 120 :family "Roboto Mono"))))
+ '(markdown-inline-code-face ((t (:inherit markdown-code-face :height 120 :family "Roboto Mono")))))
