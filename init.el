393ff24ea9c6738cf89dab636dddc7886747d94a
Florian Winkelbauer <mail@florianwinkelbauer.com>
2018-10-14 09:56:39 +0200

    Restructure configuration using MELPA
    - Add MELPA as a package manager
    - Add configuration to work with code in org-mode
    - Add magit
    - Restructure README.org

--- init.el
+++ init.el
@@ -1,3 +1,22 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
 (package-initialize)
 
-(org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
+(org-babel-load-file "~/.emacs.d/README.org")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (color-theme-sanityinc-tomorrow neotree markdown-mode magit))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
