229b6dde82cc270ed2f140aa61e579ed9e6b03a9
Florian Winkelbauer <mail@florianwinkelbauer.com>
2018-11-28 19:12:15 +0100

    Add the counsel package

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (ivy editorconfig gruvbox-theme color-theme-sanityinc-tomorrow neotree markdown-mode magit))))
+    (counsel ivy editorconfig gruvbox-theme color-theme-sanityinc-tomorrow neotree markdown-mode magit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
