3c6cc1c516b07e1a2a5a2f1c88f44d4293d18ae3
Florian Winkelbauer <mail@florianwinkelbauer.com>
2019-10-08 17:02:59 +0200

    Fix init.el

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (expand-region doom-modeline company elfeed counsel ivy editorconfig color-theme-sanityinc-tomorrow neotree markdown-mode magit)))
+    (doom-modeline company elfeed counsel ivy color-theme-sanityinc-tomorrow neotree markdown-mode magit)))
  '(safe-local-variable-values (quote ((eval flyspell-buffer)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
