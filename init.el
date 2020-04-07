6c7aafbbf13555d01a73efb68176916c2f1551e9
Florian Winkelbauer <mail@florianwinkelbauer.com>
2020-04-07 19:23:36 +0200

    Migrate from melpa stable to melpa

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+             '("melpa" . "http://melpa.org/packages/") t)
 
 (package-initialize)
 
