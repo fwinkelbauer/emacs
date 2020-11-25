bba7d272b187cc1e94ce7bd73dcbf1d609ac60e5
Florian Winkelbauer <mail@florianwinkelbauer.com>
2020-11-25 15:13:37 +0100

    Update init.el

--- init.el
+++ init.el
@@ -13,9 +13,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (avy whole-line-or-region hydra csharp-mode doom-modeline company elfeed counsel ivy color-theme-sanityinc-tomorrow neotree markdown-mode magit)))
- '(safe-local-variable-values (quote ((eval flyspell-buffer)))))
+   '(hydra csharp-mode doom-modeline company elfeed counsel ivy color-theme-sanityinc-tomorrow neotree markdown-mode magit))
+ '(safe-local-variable-values '((eval flyspell-buffer))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
