aa7bff6d923476f92d776ae9fc14b7f552a6596e
Florian Winkelbauer <mail@florianwinkelbauer.com>
2021-05-25 16:00:48 +0200

    Update magit

---
+++ elpa/transient-20210524.1637/transient-pkg.el
@@ -0,0 +1,12 @@
+(define-package "transient" "20210524.1637" "Transient commands"
+  '((emacs "25.1"))
+  :commit "eb28c513a8ad0dcd6c622dc06214d40738841d0b" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("bindings")
+  :url "https://github.com/magit/transient")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
