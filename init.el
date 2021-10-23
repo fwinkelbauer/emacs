507afb3c76ccce70bb332ac9a58305d74fc80055
Florian Winkelbauer <mail@florianwinkelbauer.com>
2021-10-23 21:40:33 +0200

    Improve transient bindings

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(undo-tree csharp-mode powershell doom-modeline company elfeed counsel ivy color-theme-sanityinc-tomorrow markdown-mode magit))
+   '(csharp-mode powershell doom-modeline company elfeed counsel ivy color-theme-sanityinc-tomorrow markdown-mode magit))
  '(safe-local-variable-values '((eval flyspell-buffer))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
