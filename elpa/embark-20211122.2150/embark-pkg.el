6a47059d75da799b850ef31922f315ab1c3f5c39
Florian Winkelbauer <mail@florianwinkelbauer.com>
2021-12-02 19:52:28 +0100

    Update packages

---
+++ elpa/embark-20211122.2150/embark-pkg.el
@@ -0,0 +1,12 @@
+(define-package "embark" "20211122.2150" "Conveniently act on minibuffer completions"
+  '((emacs "26.1"))
+  :commit "e0057f6a6621b24e0ba4c167b683a84039c6f084" :authors
+  '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
+  :maintainer
+  '("Omar Antolín Camarena" . "omar@matem.unam.mx")
+  :keywords
+  '("convenience")
+  :url "https://github.com/oantolin/embark")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
