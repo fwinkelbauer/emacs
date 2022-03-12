3b269a40684aa6b2f82c3697ae8f88f2fba681bd
Florian Winkelbauer <mail@florianwinkelbauer.com>
2022-03-12 22:11:28 +0100

    Update packages

---
+++ elpa/embark-consult-20220309.1530/embark-consult-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from embark-consult-20220309.1530/embark-consult.el  -*- no-byte-compile: t -*-
+(define-package "embark-consult" "20220309.1530" "Consult integration for Embark" '((emacs "26.1") (embark "0.12") (consult "0.10")) :commit "5faf1389162dd64bfe3511dfb8f52c18efb5140b" :authors '(("Omar Antolín Camarena" . "omar@matem.unam.mx")) :maintainer '("Omar Antolín Camarena" . "omar@matem.unam.mx") :keywords '("convenience") :url "https://github.com/oantolin/embark")
